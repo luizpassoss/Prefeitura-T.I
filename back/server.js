469319cf6b591b897a6ff09366fd0216238e6f87
luizpassoss <136658114+luizpassoss@users.noreply.github.com>
2026-01-27 12:20:48 -0300

    Reorganize frontend assets

--- back/server.js
+++ back/server.js
@@ -17,7 +17,7 @@ const publicPath = path.join(__dirname, '..', 'front', 'public');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(publicPath, 'global.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 // ROTAS API
